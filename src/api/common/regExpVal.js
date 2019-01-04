3a6ca30ec25ccfff51cf96dd076ea5dfae32bbcf
daydayhu <dengnideguoqu@163.com>
2019-01-04 18:06:05 +0800

    删除代码

--- src/api/common/regExpVal.js
+++
@@ -1,7 +0,0 @@
-const $RN = {
-  // username
-  username: /^[a-zA-Z0-9_!@#$%^&\*\(\)\_\+\-\[\]\.\?\{\}=￥·$]{1,30}$/,
-  // password
-  password: /^[a-zA-Z0-9_!@#$%^&\*\(\)\_\+\-\[\]\.\?\{\}=￥·$]{8,16}$/,
-};
-export default $RN;
